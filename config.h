3799506c0b3792dfc27147d7ca709bfacf447ec2
aklk1ng <2628084745@qq.com>
2022-11-04 16:58:45 +0800

    change the font form Jetbrains to Hack

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono Nerd Font:pixelsize=34:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font:pixelsize=34:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
