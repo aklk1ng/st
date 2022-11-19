5eae12fcba0aaca3c6d6f45bac05d493acdca74a
aklk1ng <2628084745@qq.com>
2022-11-19 15:53:34 +0800

    update map

--- config.h
+++ config.h
@@ -1,5 +1,4 @@
-/*
- * appearance
+/* appearance
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
@@ -189,8 +188,8 @@ static uint forcemousemod = ShiftMask;
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ XK_NO_MOD,            Button4, kscrollup,      {.i = 1}         },
-	{ XK_NO_MOD,            Button5, kscrolldown,    {.i = 1}         },
+	/* { XK_NO_MOD,            Button4, kscrollup,      {.i = 1}         }, */
+	/* { XK_NO_MOD,            Button5, kscrolldown,    {.i = 1}         }, */
 };
 
 static Shortcut shortcuts[] = {
